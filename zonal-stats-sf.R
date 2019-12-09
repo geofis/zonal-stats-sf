4fc3ca440097c9f52dbead7dbe7f4eb6aa5dbf3d
Jose Ramon <joseramon@geografiafisica.org>
2019-12-09 17:15:40 -0400

    added zonal statistics for density of linestrings

--- zonal-stats-sf.R
+++ zonal-stats-sf.R
@@ -11,6 +11,7 @@ zstatsf <- function(x = NULL, y = NULL, grpx = NULL, grpy = NULL) {
   suppressWarnings({
     require(sf)
     require(tidyverse)
+    require(purrr)
     
     xsf <- st_read(x, quiet = T)
     ysf <- st_read(y, quiet = T)
@@ -18,16 +19,21 @@ zstatsf <- function(x = NULL, y = NULL, grpx = NULL, grpy = NULL) {
     xsf <- xsf %>% rename(varx = contains(grpx))
     ysf <- ysf %>% rename(vary = matches(grpy))
     
+    names(ysf)[grep('geom', names(ysf))] <- 'geom'
+    st_geometry(ysf) <- 'geom'
+    poly <- any(grepl('POLY', class(ysf$geom)))
+    
     inters <- xsf %>%
+      mutate(zarea = st_area(.)) %>% 
       st_intersection(ysf %>% select(vary)) %>% 
-      mutate(area=st_area(.)) %>% 
-      group_by(varx) %>%
-      mutate(pctarea=area/sum(area)*100) %>%
-      dplyr::select(varx, vary, 'pctarea') %>%
+      mutate(area = st_area(.), len = st_length(.)) %>%
+      group_by(varx) %>% 
+      mutate(zs = case_when(poly ~ area/zarea*100, !poly ~ len/zarea*1000000)) %>% 
+      dplyr::select(varx, vary, 'zs') %>%
       group_by(varx, vary) %>%
-      summarise(pctarea=sum(pctarea)) %>%
+      summarise(zs=sum(zs)) %>%
       st_drop_geometry() %>%
-      spread(vary, pctarea, fill = 0) %>%
+      spread(vary, zs, fill = 0) %>%
       ungroup() %>% 
       inner_join(xsf %>% select(varx), by = 'varx') %>%
       rename(!!sym(grpx):=varx) %>% 
